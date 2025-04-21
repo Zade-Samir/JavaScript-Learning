c76c3fda0505f12946bcb340401b9fce90cb1ced
Samir Zade <samirzadeofficial43@gmail.com>
2025-04-21 16:14:58 +0000

    datatypes added

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all the js code as newer version
+
+// alert(3+3) //we are using node.js, so don't have to run on the browser
+
+// number => integer numbers
+// bigint
+// String => "value"
+// boolean => true/false
+// null => standalone value/also called as type of object
+//undefiend => which is not defined
+//symbols => unique values used in react.js
+
+console.log(typeof null); //object type
+console.log(typeof undefined); //undefined type
