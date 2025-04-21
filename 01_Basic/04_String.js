8f4ba9a7f7275e88b63003c67044232dfe9f6353
Samir Zade <samirzadeofficial43@gmail.com>
2025-04-21 17:31:17 +0000

    Strings added

---
+++ 01_Basic/04_String.js
@@ -0,0 +1,16 @@
+const variableName = "JavaScript"
+
+// console.log(variableName)
+// console.log(variableName[0])
+
+// console.log(variableName.charAt(4)); //finding the character at certain position
+// console.log(variableName.indexOf("S")); //finding the position of certain character
+// console.log(variableName.indexOf("s"));
+
+// ----------------------------------------------------------
+
+const score = 400
+
+console.log(score.toString());
+console.log(score.toFixed(2));
+
